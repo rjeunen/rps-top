a4f7c2c291d7a9217990b7b56da78b4c118e70fb
Robby Jeunen <robbyjeunen@gmail.com>
2025-09-29 15:52:08 +0200

    function to get computer choice & tested

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log("Testing");
+//choice of the computer
+function getComputerChoice(){
+    let computerChoice = Math.floor(Math.random() * 3) + 1;
+
+    if(computerChoice === 1){
+        computerChoice = "rock";
+    }
+    else if(computerChoice === 2){
+        computerChoice = "paper";
+    }
+    else if(computerChoice === 3){
+        computerChoice = "scissors";
+    }
+    else{
+        console.log("Something went wrong with the Random generater - function getComputerChoice()");
+    }
+
+    console.log(computerChoice);
+}
+
+getComputerChoice(); //testing
